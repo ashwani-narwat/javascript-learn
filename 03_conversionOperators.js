be4ce27c228427e71353172ef175e27f43d6722e
Ashwani Narwat <ashwaninarwat19013005@gmail.com>
2025-07-10 19:39:55 +0530

    conversion of operators

---
+++ 03_conversionOperators.js
@@ -0,0 +1,19 @@
+let score= "33"
+console.log(typeof score);
+
+let valueInNumber =Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/* conversion to number
+    "33" => 33
+    "33abc" => NaN
+    true => 1 ; false=>0
+*/
+let loggenIn="ash"
+
+let boolloggedIn = Boolean(loggenIn)
+console.log(boolloggedIn);
+/* 1=>true; 0=>false
+    "ash" => true ; "" =>false
+*/
