fe7c977de7959010d24477d2cb86410d869f4294
Ashwani Narwat <ashwaninarwat19013005@gmail.com>
2025-07-10 19:59:47 +0530

    conversion of operation done\

--- 03_conversionOperators.js
+++ 03_conversionOperators.js
@@ -17,3 +17,22 @@ console.log(boolloggedIn);
 /* 1=>true; 0=>false
     "ash" => true ; "" =>false
 */
+
+
+//************Operations*************
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+//console.log(2+2); : addition
+//console.log(2-2); : subtraction
+//console.log(2*2); : multiplication
+//console.log(2**3); : power
+//console.log(2/3); : division
+//console.log(2%3); modulus: 
+
+console.log("1"+2+2)
+// prints 122 as 1 is string so everthing is considered string
+console.log(1+2+"2")
+// prints 32 as firsts are numbers then string
+
