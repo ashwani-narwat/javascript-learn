2db713dc5e12a6081e1baf526a163f071594f777
Ashwani Narwat <ashwaninarwat19013005@gmail.com>
2025-07-13 12:22:20 +0530

    done

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,15 @@
+ const score = 400
+ const balance = new Number(100)
+ console.log(balance);
+
+ 
+
+
+
+
+ //--------------------------------------------------------------------------------------------//
+ //MATHS//
+
+ console.log(Math.abs(-4));
+ 
+ 
