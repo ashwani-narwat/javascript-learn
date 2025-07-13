7e600ad4fc53c2b4bf5bfb18f81b3fd554471a9b
Ashwani Narwat <ashwaninarwat19013005@gmail.com>
2025-07-13 11:54:09 +0530

    strings done

---
+++ 01_basics/05_string.js
@@ -0,0 +1,11 @@
+const name ="Ashwani Narwat"
+const repoCount= 50
+
+//console.log(name + repoCount + "value");  not a good way 
+console.log(`Hello my name is ${name} and my repo Count is ${repoCount}`);
+// ${}: string interpolation good way to concatenate strings
+
+const gameName= new String("hitesh")
+console.log(gameName.length);
+ 
+
